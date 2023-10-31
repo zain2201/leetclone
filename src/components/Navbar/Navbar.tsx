89eaeb39529ccb28425cdbae5dbc4233d558f1ee
zain2201 <zain90651@gmail.com>
2023-10-31 23:56:22 +0530

    add login page

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+      <Link href="/" className="flex items-center justify-center h-20">
+        <img src="/logo.png" className="h-full"></img>
+      </Link>
+      <div className="flex items-center">
+        <button className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium hover:text-brand-orange hover:bg-white hover:border-brand-orange border-2 border-transparent transition-duration-300 ease-in-out">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
